63b0dd957eeee39f9f60f46293837bc799116865
GiacomoRuscelli <giacomo.ruscelli2@studio.unibo.it>
2025-08-03 19:48:39 +0200

    Primo test  con mappa Google Maps

--- app/src/main/java/com/example/appcorsosistemimobile/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/appcorsosistemimobile/ui/screens/MapScreen.kt
@@ -1,17 +1,30 @@
 package com.example.appcorsosistemimobile.ui.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
 @Composable
 fun MapScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val startPosition = LatLng(44.1480616125233, 12.23551531160112) // Campus cesena
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(startPosition, 5f)
+    }
+
+    GoogleMap(
+        modifier = Modifier,
+        cameraPositionState = cameraPositionState
     ) {
-        Text("Mappa dei punti di immersione")
+        Marker(
+            state = rememberMarkerState(position = startPosition),
+            title = "Campus Cesena",
+            snippet = "Punto di immersione fittizio"
+        )
     }
 }
